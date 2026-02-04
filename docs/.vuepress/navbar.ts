deacad1a5edd2c804d4f128bb35feae5c1651c5d
Kauo7420 <2673861652@qq.com>
2026-02-04 18:37:51 +0800

    Update navbar.ts

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -9,11 +9,4 @@ import { defineNavbarConfig } from 'vuepress-theme-plume'
 export default defineNavbarConfig([
   { text: '首页', link: '/' },
   { text: '文档', link: '/guide/intro/' },
-  { text: '博客', link: '/blog/' },
-  { text: '标签', link: '/blog/tags/' },
-  { text: '归档', link: '/blog/archives/' },
-  {
-    text: '笔记',
-    items: [{ text: '示例', link: '/demo/README.md' }]
-  },
 ])
